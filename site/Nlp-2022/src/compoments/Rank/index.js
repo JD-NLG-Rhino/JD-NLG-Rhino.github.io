16ae88ef608dd7d844ed5ff8d27ab6d5a3c78df6
jinzenan3 <jinzenan3@jd.com>
2022-03-18 14:32:01 +0800

    add: 丰富配置化

--- site/Nlp-2022/src/compoments/Rank/index.js
+++ site/Nlp-2022/src/compoments/Rank/index.js
@@ -1,5 +1,6 @@
 import { Table } from "antd";
-import rankData from '../../assests/out.json'
+import { RANK_LABEL, RANK_IS_SORT } from "../../assests/constant";
+import rankData from "../../assests/out.json";
 import "./index.scss";
 
 const getTableColumns = () => {
@@ -7,45 +8,60 @@ const getTableColumns = () => {
     {
       title: "Rank",
       dataIndex: "Rank",
-      align:'center',
-      render:(text,record)=>{
+      align: "center",
+      render: (text, record) => {
         return (
-         <>
-          <strong>{text}</strong>
-          <div className="update-time">{record.updateTime}</div>
+          <>
+            <strong>{text}</strong>
+            <div className="update-time">{record.updateTime}</div>
           </>
-
-        )
-      }
+        );
+      },
     },
     {
       title: "Model",
       dataIndex: "Model",
-      align:'center'
+      align: "center",
     },
     {
       title: "textual_cloze",
       dataIndex: "textual_cloze",
-      align:'center',
-      sorter: (a, b) => a.textual_cloze - b.textual_cloze,
+      align: "center",
+      ...(RANK_IS_SORT
+        ? {
+            sorter: (a, b) => a.textual_cloze - b.textual_cloze,
+          }
+        : {}),
     },
     {
       title: "visual_cloze",
       dataIndex: "visual_cloze",
-      align:'center',
-      sorter: (a, b) => a.visual_cloze - b.visual_cloze,
+      align: "center",
+      ...(RANK_IS_SORT
+        ? {
+            sorter: (a, b) => a.visual_cloze - b.visual_cloze,
+          }
+        : {}),
     },
     {
       title: "visual_coherence",
       dataIndex: "visual_coherence",
-      align:'center',
-      sorter: (a, b) => a.visual_coherence - b.visual_coherence,
+      align: "center",
+      ...(RANK_IS_SORT
+        ? {
+            sorter: (a, b) => a.visual_coherence - b.visual_coherence,
+          }
+        : {}),
     },
     {
       title: "visual_ordering",
       dataIndex: "visual_ordering",
-      align:'center',
-      sorter: (a, b) => a.visual_ordering - b.visual_ordering,
+      align: "center",
+      ...(RANK_IS_SORT
+        ? {
+            sorter: (a, b) => a.visual_ordering - b.visual_ordering,
+          }
+        : {}),
     },
   ];
 };
@@ -53,9 +69,13 @@ const getTableColumns = () => {
 const Rank = () => {
   return (
     <div className="rank">
-      <div className="title">Leaderboard</div>
+      <div className="title">{RANK_LABEL}</div>
       <div className="table">
-        <Table  columns={getTableColumns()} dataSource={rankData} pagination={false} />
+        <Table
+          columns={getTableColumns()}
+          dataSource={rankData}
+          pagination={false}
+        />
       </div>
     </div>
   );
