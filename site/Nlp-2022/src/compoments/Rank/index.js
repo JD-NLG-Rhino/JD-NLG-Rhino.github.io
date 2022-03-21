0d3d2b5d75504011ae14a43005059788f4e03aa4
jinzenan3 <jinzenan3@jd.com>
2022-03-21 17:25:39 +0800

    feat：rank排行图表排行新增列

--- site/Nlp-2022/src/compoments/Rank/index.js
+++ site/Nlp-2022/src/compoments/Rank/index.js
@@ -1,11 +1,11 @@
 import { Table } from "antd";
 import { RANK_LABEL, RANK_IS_SORT } from "../../assests/config";
 import rankData from "../../assests/rank.json";
-import {tranferRankList} from './util'
+import { tranferRankList,EMPTY_TEXT } from "./util";
 import "./index.scss";
 
 const getTableColumns = () => {
-  const align='center'
+  const align = "center";
   return [
     {
       title: "Rank",
@@ -24,45 +24,122 @@ const getTableColumns = () => {
       title: "Model",
       dataIndex: "model",
       align,
-      // width:300
+      render:(text,record)=>{
+        return <div>
+          <div>{record.description}</div>
+          <div>{record.organization}</div>
+        </div>
+      }
     },
     {
-      title: "ROUGE-1",
-      dataIndex: "ROUGE1",
+      title: "Cases&Bags",
+      dataIndex: "cases_bags",
       align,
-      ...(RANK_IS_SORT
-        ? {
-            sorter: (a, b) => a.ROUGE1 - b.ROUGE1,
+      children: [
+        {
+          title: "R1",
+          dataIndex: "cases_bags_R1",
+          key: "cases_bags_R1",
+          align,
+          render:(text,record)=>{
+            return <div>{record?.cases_bags?.scores['ROUGE-1']||EMPTY_TEXT}</div>
           }
-        : {}),
-    },
-    {
-      title: "ROUGE-2",
-      dataIndex: "ROUGE2",
-      align,
-      ...(RANK_IS_SORT
-        ? {
-            sorter: (a, b) => a.ROUGE2 - b.ROUGE2,
+        },
+        {
+          title: "R2",
+          dataIndex: "cases_bags_R2",
+          key: "cases_bags_R2",
+          align,
+          render:(text,record)=>{
+            return <div>{record?.cases_bags?.scores['ROUGE-2']||EMPTY_TEXT}</div>
           }
-        : {}),
-    },
-    {
-      title: "ROUGE-L",
-      dataIndex: "ROUGEL",
-      align,
-      ...(RANK_IS_SORT
-        ? {
-            sorter: (a, b) => a.ROUGEL - b.ROUGEL,
+        },
+        {
+          title: "R3",
+          dataIndex: "cases_bags_R3",
+          key: "cases_bags_R3",
+          align,
+          render:(text,record)=>{
+            return <div>{record?.cases_bags?.scores['ROUGE-L']||EMPTY_TEXT}</div>
           }
-        : {}),
+        },
+      ],
     },
     {
-      title: "ROUGE-AVG",
-      dataIndex: "ROUGEAVG",
+      title: "Home Appliances",
+      dataIndex: "home_appliances",
+      align,
+      children:[
+        {
+          title: "R1",
+          dataIndex: "home_appliances_R1",
+          key: "home_appliances_R1",
+          align,
+          render:(text,record)=>{
+            return <div>{record?.home_appliances?.scores['ROUGE-1']||EMPTY_TEXT}</div>
+          }
+        },
+        {
+          title: "R2",
+          dataIndex: "home_appliances_R2",
+          key: "home_appliances_R2",
+          align,
+          render:(text,record)=>{
+            return <div>{record?.home_appliances?.scores['ROUGE-2']||EMPTY_TEXT}</div>
+          }
+        },
+        {
+          title: "R3",
+          dataIndex: "home_appliances_R3",
+          key: "home_appliances_R3",
+          align,
+          render:(text,record)=>{
+            return <div>{record?.home_appliances?.scores['ROUGE-L']||EMPTY_TEXT}</div>
+          }
+        },
+      ],
+    },
+    {
+      title: "Cloothing",
+      dataIndex: "clothing",
+      align,
+      children:[
+        {
+          title: "R1",
+          dataIndex: "clothing_R1",
+          key: "clothing_R1",
+          align,
+          render:(text,record)=>{
+            return <div>{record?.clothing?.scores['ROUGE-1']||EMPTY_TEXT}</div>
+          }
+        },
+        {
+          title: "R2",
+          dataIndex: "clothing_R2",
+          key: "clothing_R2",
+          align,
+          render:(text,record)=>{
+            return <div>{record?.clothing?.scores['ROUGE-2']||EMPTY_TEXT}</div>
+          }
+        },
+        {
+          title: "R3",
+          dataIndex: "clothing_R3",
+          key: "clothing_R3",
+          align,
+          render:(text,record)=>{
+            return <div>{record?.clothing?.scores['ROUGE-L']||EMPTY_TEXT}</div>
+          }
+        },
+      ],
+    },
+    {
+      title: "AVG",
+      dataIndex: "score_avg",
       align,
       ...(RANK_IS_SORT
         ? {
-            sorter: (a, b) => a.ROUGEAVG - b.ROUGEAVG,
+            sorter: (a, b) => a.score_avg - b.score_avg,
           }
         : {}),
     },
--- site/Nlp-2022/src/compoments/Rank/util.js
+++ site/Nlp-2022/src/compoments/Rank/util.js
@@ -8,23 +8,19 @@
  * ROUGEL
  */
 
-const EMPTY_TEXT='-'
+export const EMPTY_TEXT='-'
 
 export const tranferRankList=(rankData)=>{
     const {leaderboard=[]}=rankData
     const rankList =leaderboard
-    // console.log(rankList,'ranklist')
+    console.log(rankList,'ranklist')
     return rankList.map((item,index)=>{
-        const {submission,scores}=item
+        const {created}=item
         return {
             ...item,
             rank:index+1,
-            updateTime:new Date(submission.created).toLocaleDateString(),
-            model:submission.description,
-            ROUGE1:scores['ROUGE-1']||EMPTY_TEXT,
-            ROUGE2:scores['ROUGE-2']||EMPTY_TEXT,
-            ROUGEL:scores['ROUGE-L']||EMPTY_TEXT,
-            ROUGEAVG:scores['ROUGE-AVG']||EMPTY_TEXT
+            updateTime:new Date(created).toLocaleDateString(),
+ 
 
         }
     })
