afaf9601c983273889391e7aaadf251943f8f569
jinzenan3 <jinzenan3@jd.com>
2022-03-21 14:58:33 +0800

    test: 小屏适配

--- site/Nlp-2022/src/app.js
+++ site/Nlp-2022/src/app.js
@@ -2,6 +2,7 @@ import Header from "./compoments/Header"
 import Desc from "./compoments/Desc"
 import Rank from "./compoments/Rank"
 import './app.scss'
+import './iphone.scss'
 
 const Nlp2020=()=>{
   return <div className="nlp-2020">
