92e938e26cf12bbd090b6d6c92775255bd5d4c1c
ypnlp <17181347@qq.com>
2022-03-22 20:22:31 +0800

    rename

---
+++ site/CEPSUM-2.0/src/assests/config.old.js
@@ -0,0 +1,44 @@
+export const PROJECT_TITLE = "NLPCC 2022 Shared Task"; //  项目标题
+export const PROJECT_TITLE_DESC = "JD Multimodal Product Summarization Challenge"; // 项目标题说明文案
+
+export const HEADER_BG_IMG =require('./images/header-bg.png'); // 顶部背景图
+
+export const RANK_LABEL = "Leaderboard"; // 排行榜 label
+
+export const RANK_IS_SORT = false; // 排行榜指标是否支持排序
+
+/**
+ * title 标题
+ * desc 文本话术
+ * downList 下载按钮list  title：标题  url: 地址
+ */
+export const DESC_LIST = [
+  {
+    title: "What is JDMPSD?",
+    desc: "<p> <span><b>JD M</b>ultimodal <b>P</b>roduct <b>S</b>ummarization <b>D</b>ataset (JDMPSD) </span>is a dataset collected from JD.COM, a mainstream Chinese e-commerce platform. Each sample is a (product textual description, product knowledge base, product image, product summary) tuple. The dataset contains three product categories, including Cases&Bags, Home Appliances, and Clothing.</p>",
+    downList: [
+      {
+        title: "Explore JDMPSD and model predictions",
+        url: "https://rajpurkar.github.io/SQuAD-explorer/explore/v2.0/dev/",
+      },
+      {
+        title: "Explore JDMPSD and model predictions",
+        url: "https://rajpurkar.github.io/SQuAD-explorer/explore/v2.0/dev/",
+      },
+    ],
+  },
+  {
+    title: "What is JDMPSD?",
+    desc: "<p> <span><b>JD M</b>ultimodal <b>P</b>roduct <b>S</b>ummarization <b>D</b>ataset (JDMPSD) </span>is a dataset collected from JD.COM, a mainstream Chinese e-commerce platform. Each sample is a (product textual description, product knowledge base, product image, product summary) tuple. The dataset contains three product categories, including Cases&Bags, Home Appliances, and Clothing.</p>",
+    downList: [
+      {
+        title: "Explore JDMPSD and model predictions",
+        url: "https://rajpurkar.github.io/SQuAD-explorer/explore/v2.0/dev/",
+      },
+      {
+        title: "Explore JDMPSD and model predictions",
+        url: "https://rajpurkar.github.io/SQuAD-explorer/explore/v2.0/dev/",
+      },
+    ],
+  },
+];
